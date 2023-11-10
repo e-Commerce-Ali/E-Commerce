7d712bb7c83815622dd1c69380520e65f7b98359
alirdurak <durakaliriza17@gmail.com>
2023-11-10 11:15:36 +0300

    mui color palette created

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
-import './App.css'
-import Header from './components/Header/Header'
+import { createTheme, ThemeProvider } from "@mui/material";
+import "./App.css";
+import Header from "./components/Header/Header";
+const theme = createTheme({
+  palette: {
+    text: {
+      light: "#616160",
+      dark: "#5C5B5B",
+      dark2: "#514f4d",
+      footer: "#7f7f7f",
+    },
+    bg: {
+      pink: "#E1D4C2",
+      green: "#C9C9C9",
+      white: "#ffffff",
+      darkPink: "#E1D8CB",
+    },
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Header />
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
