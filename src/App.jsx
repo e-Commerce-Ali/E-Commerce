2df511d51ebb90bd85fa37e198b929e3a90d3bdc
alirdurak <durakaliriza17@gmail.com>
2023-11-11 16:11:00 +0300

    header

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ const theme = createTheme({
       dark: "#5C5B5B",
       dark2: "#514f4d",
       footer: "#7f7f7f",
+      white: "#ffffff",
     },
     bg: {
       pink: "#E1D4C2",
       green: "#C9C9C9",
       white: "#ffffff",
       darkPink: "#E1D8CB",
+      footer: "#F8F4EF",
     },
   },
 });
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,33 +1,69 @@
-import React from 'react'
-import SearchIcon from '@mui/icons-material/Search';
-import { TextField } from '@mui/material';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Box, Container, Stack, Typography } from "@mui/material";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
+const navs = [
+  { name: "Anasayfa", url: "/" },
+  { name: "Anasayfa", url: "/" },
+  { name: "Anasayfa", url: "/" },
+  { name: "Anasayfa", url: "/" },
+];
 function Header() {
-    return (
-        <div className='header'>
-            <div className="header-left-side"></div>
-            <div className="header-mid-side">
-                <SearchIcon />
-                <TextField id="standard-basic" label="Aramak istediğiniz ürünü yazınız." fullWidth />
-            </div>
-            <div className="header-right-side">
-                <a>
-                    <PersonOutlineOutlinedIcon />
-                    <p>Giriş Yap</p>
-                </a>
-                <a>
-                    <FavoriteBorderOutlinedIcon />
-                    <p>Favorilerim</p>
-                </a>
-                <a>
-                    <ShoppingCartOutlinedIcon />
-                    <p>Sepetim</p>
-                </a>
-            </div>
-        </div>
-    )
+  return (
+    <Box width="100%" height="100%">
+      <Stack
+        height="2rem"
+        width="100% "
+        bgcolor="bg.pink"
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography color="text.white" variant="subtitle2">
+          Pasha Deri
+        </Typography>
+      </Stack>
+      <Stack
+        justifyContent="space-evenly"
+        direction="row"
+        bgcolor="bg.white"
+        width="100%"
+        height="5rem"
+      >
+        <Stack
+          justifyContent="center"
+          alignItems="center"
+          direction="row"
+          gap=".5rem"
+        >
+          {navs.map((i, j) => {
+            return (
+              <Typography
+                sx={{ cursor: "pointer" }}
+                fontSize=".9rem"
+                color="text.dark"
+                key={j}
+              >
+                {i.name}
+              </Typography>
+            );
+          })}
+        </Stack>
+        <Stack>
+          <img
+            style={{ height: "100%", cursor: "pointer" }}
+            src="https://openclipart.org/image/800px/68953"
+            alt=""
+          />
+        </Stack>
+        <Stack direction="row" justifyContent="center" alignItems="center">
+          <SearchOutlinedIcon color="text.dark"></SearchOutlinedIcon>
+          <PersonOutlineOutlinedIcon></PersonOutlineOutlinedIcon>
+          <ShoppingBagOutlinedIcon></ShoppingBagOutlinedIcon>
+        </Stack>
+      </Stack>
+    </Box>
+  );
 }
 
-export default Header
+export default Header;
