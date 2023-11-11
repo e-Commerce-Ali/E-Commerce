93651a7647608a22ceb4c21cdf6f3e0bc7d3e791
ademkaragulle <ademkaragulle@outlook.com>
2023-11-11 16:03:15 +0300

    add footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from 'react'
-import './App.css'
-import Footer from './components/footer/Footer'
+import { createTheme, ThemeProvider } from "@mui/material";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/footer/Footer";
+const theme = createTheme({
+  palette: {
+    text: {
+      light: "#616160",
+      dark: "#5C5B5B",
+      dark2: "#514f4d",
+      footer: "#7f7f7f",
+    },
+    bg: {
+      pink: "#E1D4C2",
+      green: "#C9C9C9",
+      white: "#ffffff",
+      darkPink: "#E1D8CB",
+      footer: "#F8F4EF"
+    },
+  },
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <div style={{ height: '500px' }}></div>
       <Footer />
-    </>
-  )
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,27 +1,66 @@
 import React from 'react'
 import './Footer.css'
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
 
 function Footer() {
+
+
+    const year = new Date()
+
+
     return (
         <div className='footer'>
-            <div className="row">
+            <div className="row top-footer">
                 <div className='col-md-4'>
-                    <h3>Pasha Deri</h3>
+                    <h3>PASHA DERİ</h3>
                     <ul>
-                        <li>Anasayfa</li>
-                        <li>Kategori</li>
-                        <li>Hakkımızda</li>
-                        <li>İletişim</li>
+                        <li><a href="">Anasayfa</a></li>
+                        <li><a href="">Kategori</a></li>
+                        <li><a href="">Hakkımızda</a></li>
+                        <li><a href="">İletişim</a></li>
                     </ul>
                 </div>
                 <div className='col-md-4'>
-
+                    <h3>MÜŞTERİ HİZMETLERİ</h3>
+                    <ul>
+                        <li><a href="">Aydınlatma Metni</a></li>
+                        <li><a href="">Mesafeli Satış Sözleşmesi</a></li>
+                        <li><a href="">Üyelik Sözleşmesi</a></li>
+                        <li><a href="">İptal ve İade Süreci</a></li>
+                        <li><a href="">Gizlilik Politikası</a></li>
+                    </ul>
                 </div>
                 <div className='col-md-4'>
-
+                    <h3>SERVİSLER</h3>
+                    <ul>
+                        <li><a href="">Üye Girişi</a></li>
+                        <li><a href="">Yeni Üye Ol</a></li>
+                    </ul>
                 </div>
-                <div className='col-md-4'>
+                <div className='col-md-4 social-media'>
+                    <h3>SOSYAL MEDYA</h3>
+                    <ul>
+                        <li>Bizi sosyal medyada takip edin</li>
+                        <li>
+                            <a href=""><FacebookIcon fontSize='small' /></a>
+                            <a href=""><TwitterIcon fontSize='small' /></a>
+                            <a href=""><InstagramIcon fontSize='small' /></a>
+                            <a href=""><YouTubeIcon fontSize='small' /></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="row bottom-footer">
+                <div className="col-md-6">
+                    Copyright © {year.getFullYear()}, <a className='link-website' href="">www.Lorem.com</a> | Tüm Hakları Saklıdır.
+                </div>
+                <div className="col-md-6">
 
+                    Bu site <a href="https://ademkaragulle.netlify.app"><strong className='link-website'>Adem Karagülle</strong></a> tarafından yapılmıştır
                 </div>
             </div>
         </div>
